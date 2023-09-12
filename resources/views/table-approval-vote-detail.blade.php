3d7353ffd80cc5cb2f3b3cbb4ad8e8fd21ac886c
daminhieu <daminhieu173@gmail.com>
2023-09-12 17:28:30 +0700

    feature/20230912/editLayout

--- resources/views/table-approval-vote-detail.blade.php
+++ resources/views/table-approval-vote-detail.blade.php
@@ -17,14 +17,14 @@
                                     <th scope="col">Mã máy</th>
                                     <th scope="col">Tên máy</th>
                                     <th scope="col">Ca sản xuất</th>
+                                    <th scope="col">Giờ công</th>
                                     <th scope="col">Mã CBNV</th>
                                     <th scope="col">Tên nhân viên</th>
                                     <th scope="col">Mã BTP</th>
                                     <th scope="col">Loại BTP,TP</th>
-                                    <th scope="col">ĐVT</th>
-                                    <th scope="col">Giờ công</th>
-                                    <th scope="col">Lô sản xuất</th>
                                     <th scope="col">Số lượng</th>
+                                    <th scope="col">ĐVT</th>
+                                    <th scope="col">Lô sản xuất</th>
                                     <th scope="col">Hệ số</th>
                                     <th scope="col">Số lượng quy đổi</th>
                                     <th scope="col">Phế phẩm (ĐVT gốc)</th>
@@ -53,14 +53,14 @@
                                                 @endforeach
                                             </select>
                                         </td>
+                                        <td><input class="form-control" type="text" name="TimeExcute[]" value="{{number_format($item->TimeExcute,2,".",".")}}"></td>
                                         <td>{{$item->EmployeeCode}}</td>
                                         <td>{{$item->EmployeeName}}</td>
                                         <td>{{$item->ProductCode}}</td>
                                         <td>{{$item->Name}}</td>
-                                        <td>{{$item->Unit}}</td>
-                                        <td><input class="form-control" type="text" name="TimeExcute[]" value="{{number_format($item->TimeExcute,2,".",".")}}"></td>
-                                        <td>{{$item->ItemLotCode}}</td>
                                         <td><input class="form-control" type="text" name="Quantity9[]" value="{{number_format($item->Quantity9,2,".",".")}}"></td>
+                                        <td>{{$item->Unit}}</td>
+                                        <td>{{$item->ItemLotCode}}</td>
                                         <td>{{number_format($item->ConvertRate9,2,",",".")}}</td>
                                         <td>{{number_format($item->Quantity,2,",",".")}}</td>
                                         <td>{{number_format($item->QuantityFail,2,",",".")}}</td>
