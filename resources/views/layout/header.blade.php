3df26d9f0b1ce9a1484aecc099af2b97313a6d6c
daminhieu <daminhieu173@gmail.com>
2023-09-12 17:09:44 +0700

    feature/20230912/deleteCmt

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,52 +1,24 @@
 <header class="topbar" data-navbarbg="skin6">
     <nav class="navbar top-navbar navbar-expand-md navbar-light">
         <div class="navbar-header" data-logobg="skin5">
-            <!-- This is for the sidebar toggle which is visible on mobile only -->
             {{-- <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)">
                 <i class="ti-menu ti-close"></i>
             </a> --}}
-            <!-- ============================================================== -->
-            <!-- Logo -->
-            <!-- ============================================================== -->
             <div class="navbar-brand">
                 <a href="index.html" class="logo">
-                    <!-- Logo icon -->
                     <b class="logo-icon">
-                        <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
-                        <!-- Dark Logo icon -->
                         <img src="{{ asset('dist/images/logo-icon.png') }}" alt="homepage" class="dark-logo" />
-                        <!-- Light Logo icon -->
                         <img src="{{ asset('dist/images/logo-icon.png') }}" alt="homepage" class="light-logo" />
                     </b>
-                    <!--End Logo icon -->
-                    <!-- Logo text -->
                     <span class="logo-text">
-                        <!-- dark Logo text -->
                         <img src="{{ asset('dist/images/logo-text.png') }}" alt="homepage" class="dark-logo" />
-                        <!-- Light Logo text -->
                         <img src="{{ asset('dist/images/logo-light-text.png') }}" class="light-logo" alt="homepage" />
                     </span>
                 </a>
-            </div>
-            <!-- ============================================================== -->
-            <!-- End Logo -->
-            <!-- ============================================================== -->
-            <!-- ============================================================== -->
-            <!-- Toggle which is visible on mobile only -->
-            <!-- ============================================================== -->
-            
+            </div> 
         </div>
-        <!-- ============================================================== -->
-        <!-- End Logo -->
-        <!-- ============================================================== -->
         <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin6">
-            <!-- ============================================================== -->
-            <!-- toggle and nav items -->
-            <!-- ============================================================== -->
             <ul class="navbar-nav float-start me-auto">
-                <!-- ============================================================== -->
-                <!-- Search -->
-                <!-- ============================================================== -->
                 {{-- <li class="nav-item search-box">
                     <a class="nav-link waves-effect waves-dark" href="javascript:void(0)">
                         <div class="d-flex align-items-center">
@@ -64,16 +36,10 @@
                     </form>
                 </li> --}}
             </ul>
-            <!-- ============================================================== -->
-            <!-- Right side toggle and nav items -->
-            <!-- ============================================================== -->
             <ul class="navbar-nav float-end">
-                <!-- ============================================================== -->
-                <!-- User profile and search -->
-                <!-- ============================================================== -->
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="full-name-user-header">{{session()->get('user')->Name}}</span>
+                        <span class="full-name-user-header align-middle">{{session()->get('user')->Name}}</span>
                         <img src="{{ asset('dist/images/avatar.png') }}" alt="user" class="rounded-circle" width="31">
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end user-dd animated" aria-labelledby="navbarDropdown">
@@ -82,9 +48,6 @@
                         <a class="dropdown-item" href="{{ route('logout') }}"><i class="ti-wallet me-1 ms-1"></i>Đăng xuất</a>
                     </ul>
                 </li>
-                <!-- ============================================================== -->
-                <!-- User profile and search -->
-                <!-- ============================================================== -->
             </ul>
         </div>
     </nav>
