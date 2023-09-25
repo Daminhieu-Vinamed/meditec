4870dd8f6afb3e9b63853bed516c84d0e4fc34bb
daminhieu <daminhieu173@gmail.com>
2023-09-25 08:53:56 +0700

    feature/20232509/editBaseDesignParttern

--- app/Repositories/ApprovalVoteRepository.php
+++ app/Repositories/ApprovalVoteRepository.php
@@ -15,17 +15,4 @@ class ApprovalVoteRepository extends AbstractRepository
     public function getApprovalVote() {
         return vB30JobRecord_ExploeruWeb::orderBy('DocDate', 'desc')->paginate(6);
     }
-
-    public function updateStatusApprovalVote($request) {
-        DB::beginTransaction();
-        try{
-            DB::update('EXEC usp_Update_B30JobRecord ?', [$request->parentId]);
-            DB::commit();
-            return response()->json(['error_correct' => 'Duyệt phiếu thành công !']);
-        }catch(\Exception $e){
-            DB::rollBack();
-            $message = $e->getMessage();
-            return response()->json(['error_incorrect' => $message]);
-        }
-    }
 }
--- app/Services/ApprovalVoteService.php
+++ app/Services/ApprovalVoteService.php
@@ -2,6 +2,7 @@
 namespace App\Services;
 
 use App\Repositories\ApprovalVoteRepository;
+use Illuminate\Support\Facades\DB;
 
 class ApprovalVoteService extends ApprovalVoteRepository{
     protected ApprovalVoteRepository $approvalVoteRepository;
@@ -14,5 +15,18 @@ class ApprovalVoteService extends ApprovalVoteRepository{
     public function getApprovalVote()
     {
         return $this->approvalVoteRepository->getApprovalVote();
+    }
+
+    public function updateStatusApprovalVote($request) {
+        DB::beginTransaction();
+        try{
+            DB::update('EXEC usp_Update_B30JobRecord ?', [$request->parentId]);
+            DB::commit();
+            return response()->json(['error_correct' => 'Duyệt phiếu thành công !']);
+        }catch(\Exception $e){
+            DB::rollBack();
+            $message = $e->getMessage();
+            return response()->json(['error_incorrect' => $message]);
+        }
     }
 }
