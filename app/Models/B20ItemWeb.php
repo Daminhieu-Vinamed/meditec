8763d79e0258f90c6f7071ff3ca6b2888d75ae8a
daminhieu <daminhieu173@gmail.com>
2023-10-02 14:59:27 +0700

    feature/20230210/addColumn

---
+++ app/Models/B20ItemWeb.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class B20ItemWeb extends Model
+{
+    use HasFactory;
+    protected $table = 'B20ItemWeb';
+}
