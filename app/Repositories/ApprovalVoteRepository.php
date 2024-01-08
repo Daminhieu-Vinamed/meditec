011b034fa79bb754629f7ff121cb68b0ec9bebba
daminhieu <daminhieu173@gmail.com>
2024-01-08 10:26:29 +0700

    feature/20240801/edit-usp_Update_B30JobRecordDetailFromWeb

--- app/Repositories/ApprovalVoteRepository.php
+++ app/Repositories/ApprovalVoteRepository.php
@@ -15,9 +15,12 @@ class ApprovalVoteRepository extends AbstractRepository
     public function getDataApprovalVote() {
         $data = $this->builder()->orderBy('DocDate', 'desc')->get();
         return DataTables::of($data)
+        ->editColumn('DocDate', function($approvalVote){
+            return date("d-m-Y", strtotime($approvalVote['DocDate']));
+        })
         ->editColumn('DocNo', function($approvalVote){
             return '<a href="'.route('list.detail-approval-vote', ['id' => $approvalVote['Id']]).'">'.$approvalVote['DocNo'].'</a>';
-         })
+        })
         ->addColumn('action', function($approvalVote){
            return '<button type="button" class="btn btn-grey-light update-status-approval-vote" id="'. $approvalVote['Id'].'">DUYỆT PHIẾU</button>';
         })
